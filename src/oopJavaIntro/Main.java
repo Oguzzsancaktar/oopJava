336f447f9c543323bfcd3c466b7868a5c776f1c6
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-06-26 19:59:24 +0300

    Ok

--- src/oopJavaIntro/Main.java
+++ src/oopJavaIntro/Main.java
@@ -5,63 +5,69 @@ import java.util.Iterator;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product5 = new Product(5 , "test 5 ", 1700, "detail 5" );
+		
+		Product product5 = new Product(5,"test 5 ",1700,"detail 5",10);
 		
 		Product product1 = new Product(); // instance , referance creation
-		product1.id = 1;
-		product1.name = "test 1";
-		product1.unitPrice = 15000;
-		product1.detail = "Test Detail";
+	
 		
 		Product product2 = new Product(); // instance , referance creation
-		product2.id = 2;
-		product2.name = "test 2 ";
-		product2.unitPrice = 15000;
-		product2.detail = "Test Detail";
-		
-		Product product3 = new Product(); // instance , referance creation
-		product3.id = 3;
-		product3.name = "test 3";
-		product3.unitPrice = 15000;
-		product3.detail = "Test Detail";
-		
-		Product product4 = new Product(); // instance , referance creation
-		product4.id = 4;
-		product4.name = "test 4";
-		product4.unitPrice = 15000;
-		product4.detail = "Test Detail";
-		
-		product5.name = "owerride p5 name";
-		Product[] products = {
-				product1,
-				product2,
-				product5,
-				product4,
-				product3
-				
-		};
-		
-		for (Product product : products) {
-			System.out.println(product.id);
-		}
+		product2.setId(2);
+		product2.setName("Product 2");
+		product2.setDetail("P2 Detail");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
 		
 		
-		System.out.println(products.length);
+		
+		
+		System.out.println(product2.getUnitPriceAfterDiscount());
 		
 		
 		Category category1 = new Category();
-		category1.id = 1;
-		category1.name = "C1";
+		category1.setId(1);
+		category1.setName("Catgory 1");
 		
 		Category category2 = new Category();
-		category1.id = 2;
-		category1.name = "C2";
+		category1.setId(2);
 		
+		category1.setName("Catgory 1");
+		category2.setName("Catgory 2");
 		
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
+				
 		
-		ProductManager productManager = new ProductManager();
-		
-		productManager.addToCart(product5);
+//		Product product3 = new Product(); // instance , referance creation
+//		Product[] products = {
+//				product1,
+//				product2,
+//				product3,
+//	
+//				
+//		};
+//		
+//		for (Product product : products) {
+//			System.out.println(product.getName());
+//		}
+//		
+//		
+//		System.out.println(products.length);
+//		
+//		
+//		Category category1 = new Category();
+//		category1.id = 1;
+//		category1.name = "C1";
+//		
+//		Category category2 = new Category();
+//		category1.id = 2;
+//		category1.name = "C2";
+//		
+//		
+//		
+//		ProductManager productManager = new ProductManager();
+//		
+//		productManager.addToCart(product5);
 		
 		
 		
--- src/oopJavaIntro/ProductManager.java
+++ src/oopJavaIntro/ProductManager.java
@@ -2,6 +2,6 @@ package oopJavaIntro;
 
 public class ProductManager {
 	public void addToCart (Product product) {
-		System.out.println("added to cart " + product.name);
+		System.out.println("added to cart " + product.getName());
 	}
 }
